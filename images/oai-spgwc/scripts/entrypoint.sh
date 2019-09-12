017282b0404bad7ef70945c96f1f4643999b98dc
Frank A. Zdarsky <fzdarsky@redhat.com>
2019-09-12 10:19:19 -0400

    Additions for running oai-spgwc on podman

--- images/oai-spgwc/scripts/entrypoint.sh
+++ images/oai-spgwc/scripts/entrypoint.sh
@@ -8,6 +8,7 @@ sed -i \
     -e "s/\${PGW_S5S8_INTERFACE}/${PGW_S5S8_INTERFACE}/g" \
     -e "s/\${PGW_SX_INTERFACE}/${PGW_SX_INTERFACE}/g" \
     -e "s/\${UE_IP_ADDRESS_POOL}/${UE_IP_ADDRESS_POOL}/g" \
+    -e "s/\${UE_DNS_SERVER}/${UE_DNS_SERVER}/g" \
     /opt/oai-spgwc/etc/spgw_c.conf
 
 exec "$@"
