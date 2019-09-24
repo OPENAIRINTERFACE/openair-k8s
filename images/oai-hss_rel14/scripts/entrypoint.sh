f854caddd829311c8e773a73f7cf1540a779fd71
Frank A. Zdarsky <fzdarsky@redhat.com>
2019-09-24 12:58:37 -0400

    Adding configs to oai-hss_rel14

--- images/oai-hss_rel14/scripts/entrypoint.sh
+++ images/oai-hss_rel14/scripts/entrypoint.sh
@@ -2,34 +2,29 @@
 
 set -euo pipefail
 
+HSS_DOMAIN=${HSS_FQDN#*.}
 DB_NAME=${DB_NAME:-vhss}
-
-cat << EOF > $HOME/.my.cnf
-[client]
-password=${MYSQL_PASSWORD}
-EOF
-if ! mysql -h ${MYSQL_SERVER} -u ${MYSQL_USER} -e "use ${MYSQL_DATABASE};" 2>/dev/null; then
-    echo "Creating database ${MYSQL_DATABASE} on ${MYSQL_SERVER} and initializing tables."
-    mysql -h ${MYSQL_SERVER} -u ${MYSQL_USER} -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
-    mysql -h ${MYSQL_SERVER} -u ${MYSQL_USER} "${MYSQL_DATABASE}" < /opt/oai-hss/bin/oai_db.sql
-fi
+ROAMING_ALLOWED=${ROAMING_ALLOWED:-true}
 
 sed -i \
-    -e "s/\${DB_SERVER}/${DB_SERVER}/g" \
+    -e "s/\${DB_FQDN}/${DB_FQDN}/g" \
     /opt/oai-hss/etc/hss_rel14.conf
 sed -i \
-    -e "s/\${DB_SERVER}/${DB_SERVER}/g" \
+    -e "s/\${HSS_FQDN}/${HSS_FQDN}/g" \
+    -e "s/\${HSS_DOMAIN}/${HSS_DOMAIN}/g" \
+    -e "s/\${DB_FQDN}/${DB_FQDN}/g" \
     -e "s/\${DB_NAME}/${DB_NAME}/g" \
     -e "s/\${DB_USER}/${DB_USER}/g" \
     -e "s/\${DB_PASSWORD}/${DB_PASSWORD}/g" \
     -e "s/\${OP_KEY}/${OP_KEY}/g" \
+    -e "s/\${ROAMING_ALLOWED}/${ROAMING_ALLOWED}/g" \
     /opt/oai-hss/etc/hss_rel14.json
 sed -i \
-    -e "s/\${HSS_SERVER}/${HSS_SERVER}/g" \
-    -e "s/\${HSS_REALM}/${HSS_REALM}/g" \
-    /opt/oai-hss/etc/hss_fd.conf
+    -e "s/\${HSS_FQDN}/${HSS_FQDN}/g" \
+    -e "s/\${HSS_DOMAIN}/${HSS_DOMAIN}/g" \
+    /opt/oai-hss/etc/hss_rel14_fd.conf
 sed -i \
-    -e "s/\${HSS_REALM}/${HSS_REALM}/g" \
+    -e "s/\${HSS_DOMAIN}/${HSS_DOMAIN}/g" \
     /opt/oai-hss/etc/acl.conf
 
 exec "$@"
