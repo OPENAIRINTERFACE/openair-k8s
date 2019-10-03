b54203291929f621c83ad0c7f1a68dfe13f06c11
lionelgo <gauthier@eurecom.fr>
2019-10-03 10:11:47 +0200

    WIP on SPGWC image, typo in spgw_c.conf file

--- images/oai-spgwc/scripts/entrypoint.sh
+++ images/oai-spgwc/scripts/entrypoint.sh
@@ -4,8 +4,6 @@ set -euo pipefail
 
 sed -i \
     -e "s/\${SGW_S11_INTERFACE}/${SGW_S11_INTERFACE}/g" \
-    -e "s/\${SGW_S5S8_INTERFACE}/${SGW_S5S8_INTERFACE}/g" \
-    -e "s/\${PGW_S5S8_INTERFACE}/${PGW_S5S8_INTERFACE}/g" \
     -e "s/\${PGW_SX_INTERFACE}/${PGW_SX_INTERFACE}/g" \
     -e "s/\${UE_IP_ADDRESS_POOL}/${UE_IP_ADDRESS_POOL}/g" \
     -e "s/\${UE_DNS_SERVER}/${UE_DNS_SERVER}/g" \
