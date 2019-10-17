20998128d9f89ee6212c08af0bea8c66f6e50540
Frank A. Zdarsky <fzdarsky@redhat.com>
2019-10-17 04:49:36 -0400

    Make oai-enb and oai-ue work with multiple config files

--- images/oai-ue/scripts/entrypoint.sh
+++ images/oai-ue/scripts/entrypoint.sh
@@ -1,11 +1,33 @@
 #!/bin/bash
 
-set -euo pipefail
+set -eo pipefail # don't set 'u' as we need to explicityly test for undefined vars
 
-sed -i \
-    -e "s/\${UE_IF}/${UE_IF}/g" \
-    -e "s/\${UE_IP}/${UE_IP}/g" \
-    -e "s/\${ENB_IP}/${ENB_IP}/g" \
-    /opt/oai-ue/etc/ue.conf
+CONFIG_DIR="../etc"
+DEFAULT_MODE="RCC"
+
+# look up configuration template to use
+MODE=${MODE:-${DEFAULT_MODE}}
+case "${MODE^^}" in
+    UE) TEMPLATE=${CONFIG_DIR}/rru.tdd.band40.conf.template;;
+    *)   echo "Unkown mode '${MODE}'."; exit 1;;
+esac
+
+# grep variable names (format: ${VAR}) from template to be rendered
+VARS=$(grep -oP '\$\{\K[a-zA-Z0-9_]+' ${TEMPLATE} | sort | uniq | xargs)
+
+# create sed expressions for substituting each occurrence of ${VAR}
+# with the value of the environment variable "VAR"
+EXPRESSIONS=""
+for v in ${VARS}; do
+    if [[ "${!v}x" == "x" ]]; then
+        echo "Error: Environment variable '${v}' is not set." \
+             "Config file '$(basename $TEMPLATE)' requires all of $VARS."
+        exit 1
+    fi
+    EXPRESSIONS="${EXPRESSIONS} -e s|\\\${${v}}|${!v}|g"
+done
+
+# render template and write to ue.conf
+sed ${EXPRESSIONS} ${TEMPLATE} > ${CONFIG_DIR}/ue.conf
 
 exec "$@"
