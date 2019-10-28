4d9f16ab5eb094d47794fb836ca25a01a49c34e3
Ricardo Noriega <rnoriega@redhat.com>
2019-10-28 12:45:50 +0100

    Fix param in SPGW-u entrypoint
    Signed-off-by: Ricardo Noriega <rnoriega@redhat.com>

--- images/oai-spgwu/scripts/entrypoint.sh
+++ images/oai-spgwu/scripts/entrypoint.sh
@@ -6,7 +6,7 @@ sed -i \
     -e "s/\${SGW_S1U_INTERFACE}/${SGW_S1U_INTERFACE}/g" \
     -e "s/\${SGW_SX_INTERFACE}/${SGW_SX_INTERFACE}/g" \
     -e "s/\${PGW_SGI_INTERFACE}/${PGW_SGI_INTERFACE}/g" \
-    -e "s/\${NETWORK_UE_IP}/${NETWORK_UE_IP}/g" \
+    -e "s|\${NETWORK_UE_IP}|${NETWORK_UE_IP}|g" \
     -e "s/\${PGWC_SX_IP_ADDRESS}/${PGWC_SX_IP_ADDRESS}/g" \
     /opt/oai-spgwu/etc/spgw_u.conf
 
