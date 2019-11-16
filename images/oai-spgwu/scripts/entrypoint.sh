b7717220cd18bcd6d57974dbf519b5dec3868920
lionelgo <gauthier@eurecom.fr>
2019-11-16 17:23:26 +0100

    Change default route for SPGWU proposal, to validate

--- images/oai-spgwu/scripts/entrypoint.sh
+++ images/oai-spgwu/scripts/entrypoint.sh
@@ -25,11 +25,34 @@ for c in ${CONFIG_DIR}/*.conf; do
     sed -i "${EXPRESSIONS}" ${c}
 done
 
+# Test for routing scripts 
+for v in PGW_SGI_INTERFACE PGWU_SGI_GW NETWORK_UE_IP; do
+    if [[ "${!v}x" == "x" ]]; then
+        echo "Error: Environment variable '${v}' is not set." \
+                "required for routing scripts"
+        exit 1
+    fi
+done
+
 # This will be done by the spgwu internally soon, but for the time being:
 echo '200 lte' | tee --append /etc/iproute2/rt_tables
 # Default gateway 
-ip route add default via $PGWU_SGI_GW dev $PGW_SGI_INTERFACE table lte
+ip route add default via $PGWU_SGI_GW dev $PGW_SGI_INTERFACE table 200
 # you will have to repeat the following line for each PDN network set in your SPGW-U config file
-ip rule add from $NETWORK_UE_IP table lte
+ip rule add from $NETWORK_UE_IP table 200
+
+# Actually required since SGi may be not the interface on which is set the default route for the
+# container. Otherwize if not possible an ARP proxy could be set on the SGi next hop that is the 
+# default gw 
+DEFAULT_ROUTE=`ip route show table main | grep default`
+DEFAULT_ROUTE_GW=`echo $DEFAULT_ROUTE | cut -d ' ' -f3`
+DEFAULT_ROUTE_DEV=`echo $DEFAULT_ROUTE | cut -d ' ' -f5`
+if [ $PGW_SGI_INTERFACE != $DEFAULT_ROUTE_DEV ] || [ $PGWU_SGI_GW != $DEFAULT_ROUTE_GW ] ; then
+  echo "Removing route: $DEFAULT_ROUTE"
+  ip route del $DEFAULT_ROUTE
+  echo "Adding route: ip route add default via $PGWU_SGI_GW dev $PGW_SGI_INTERFACE"
+  ip route add default via $PGWU_SGI_GW dev $PGW_SGI_INTERFACE
+fi
+
 
 exec "$@"
